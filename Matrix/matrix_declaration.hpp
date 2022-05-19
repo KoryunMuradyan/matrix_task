e1a5364c5b1d8ee36eae55a293c3fd1731db50ff
Your Koryun <koryun1211@gmail.com>
2022-05-19 20:14:15 +0400

    changings in matrix_definition.hpp and matrix_declaration.hpp

--- Matrix/matrix_declaration.hpp
+++ Matrix/matrix_declaration.hpp
@@ -14,27 +14,11 @@ private:
 	void allocSpace();
 public:o
        Matrix obj;
-	Matrix()
-		: rows_(1)
-		, cols_(1)
-	{
-		std::vector<std::vector<int>> vec(rows_, std::vector<int>(cols_, T(NULL)));
-	}
+	Matrix();
 
-	Matrix(int& arg_rows, int& arg_cols)
-		: rows_(arg_rows)
-		, cols_(arg_cols)
-	{
-		std::vector<std::vector<int>> vec(rows_, std::vector<int>(cols_, T(NULL)));
-	}
+	Matrix(int&, int&);
 
-	Matrix(int& arg_rows, int& arg_cols, T&)
-		: rows_(arg_rows)
-		, cols_(arg_cols)
-	{
-		std::vector<std::vector<int>> vec(rows_, std::vector<int>(cols_, 0));
-	}
-
+	Matrix(int&, int&, T&);
 
 
 }
--- Matrix/matrix_definition.hpp
+++ Matrix/matrix_definition.hpp
@@ -4,6 +4,28 @@
 
 using  namespace math;
 
+
+Matrix::Matrix()
+	: rows_(1)
+	, cols_(1)
+{
+	std::vector<std::vector<int>> vec(rows_, std::vector<int>(cols_, T(NULL)));
+}
+
+Matrix::Matrix(int& arg_rows, int& arg_cols)
+	: rows_(arg_rows)
+	, cols_(arg_cols)
+{
+	std::vector<std::vector<int>> vec(rows_, std::vector<int>(cols_, T(NULL)));
+}
+
+Matrix::Matrix(int& arg_rows, int& arg_cols, T& arg_val)
+	: rows_(arg_rows)
+	, cols_(arg_cols)
+{
+	std::vector<std::vector<int>> vec(rows_, std::vector<int>(cols_, arg_val));
+}
+
 void Matrix::allocSpace()
 {
 	p = new double*[rows_];
